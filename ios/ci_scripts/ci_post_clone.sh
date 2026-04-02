5bb0bad17cf28fa48491f0de0e2cc32e50be2af7
Sebastian Sole <sebastian.sole@knowit.no>
2026-04-02 12:14:53 +0200

    fix

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
 # Install Node.js via Homebrew (available in Xcode Cloud)
 brew install node@20
 brew link node@20 --force --overwrite
@@ -9,7 +12,7 @@ brew link node@20 --force --overwrite
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 
 # Install JS dependencies
-npm install
+npm install --legacy-peer-deps
 
 # Install CocoaPods dependencies
 cd ios
