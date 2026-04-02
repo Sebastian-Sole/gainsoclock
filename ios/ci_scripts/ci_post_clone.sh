7f3e1ea5ca2f80aeae6e4dd3e6721f5755e049aa
Sebastian Sole <sebastian.sole@knowit.no>
2026-04-02 11:43:41 +0200

    fix

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -e
 
+# Install Node.js via Homebrew (available in Xcode Cloud)
+brew install node@20
+brew link node@20 --force --overwrite
+
 # Navigate to repository root
 cd "$CI_PRIMARY_REPOSITORY_PATH"
-
-# Install Node.js using nvm (Xcode Cloud has nvm pre-installed)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 20
-nvm use 20
 
 # Install JS dependencies
 npm install
